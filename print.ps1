94c922ddabe54f154605507a039a005b07807e81
Yusuke arakawa <yusu.arakawa@gmail.com>
2019-01-07 14:51:01 +0900

    add data table

--- print.ps1
+++ print.ps1
@@ -5,6 +5,8 @@ foreach($invoice in $outputs){
 
     # Print to Default Printer
     Start-Process $invoice -Verb Print | Stop-Process
+    # Print to your prefer printer
+    #Start-Process soffice.exe -ArgumentList "-p $invoce.Fullname"
     # After print and Delete
     Remove-Item $invoice
 
